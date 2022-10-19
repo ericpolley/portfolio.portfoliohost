8c1a3c5e18d953630655b75be3e5859d1c18ef3a
Eric Polley <109182529+ericpolley@users.noreply.github.com>
2022-10-19 16:29:29 -0700

    Add files via upload

--- portfolio.js
+++ portfolio.js
@@ -3,12 +3,13 @@ const portfolioBoxDisplay = document.getElementById('portfolioBox')
 const navBarDisplay = document.getElementById('navBarhover')
 const contactDisplay = document.getElementById('contactBox')
 const portfolioLinksDisplay = document.getElementById('portfolioLinks')
-
+const home = document.getElementById('home')
 const navContactButton = document.getElementById('navContact')
 const navBlogButton = document.getElementById('navBlog')
 
 start.addEventListener('click', startWebsite)
 navContactButton.addEventListener('click', displayContact)
+home.addEventListener('click', displayPortfolio)
 
 
 
@@ -29,6 +30,12 @@ function displayContact() {
     console.log('display contact')
     portfolioBoxDisplay.classList.add('hide')
     contactDisplay.classList.remove('hide')
+}
+
+function displayPortfolio() {
+    console.log('display contact')
+    portfolioBoxDisplay.classList.remove('hide')
+    contactDisplay.classList.add('hide')
 }
 
 
